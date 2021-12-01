779cf8c6e37f62852bd150685757f654903bf2fe
Mateusz.Manka <mateusz.manka@eclipsegroup.co.uk>
2021-12-01 14:20:03 +0100

    1️⃣ day 1

---
+++ src/main/kotlin/dev/manka/advent_of_code/days/y2021/Day01.kt
@@ -0,0 +1,24 @@
+package dev.manka.advent_of_code.days.y2021
+
+import dev.manka.advent_of_code.days.Day
+
+class Day01 : Day(1,"Sonar Sweep"){
+    override fun partOne(): String {
+        return inputIntSequence()
+            .countIncreses()
+            .toString()
+    }
+
+    override fun partTwo(): String {
+        return inputIntSequence()
+            .windowed(3)
+            .map{it.sum()}
+            .countIncreses()
+            .toString()
+    }
+    private fun Sequence<Int>.countIncreses() : Int{
+        return this.windowed(2)
+            .count {it.last()-it.first() > 0 }
+    }
+
+}
